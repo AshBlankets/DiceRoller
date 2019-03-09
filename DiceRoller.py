4e76e7ce5f32649f068541739d111f39d2bbcb92
Ash King <ashleighak@hotmail.com>
2019-03-09 16:59:59 +1300

    Update DiceRoller.py
    Printing coloured text

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,6 +1,10 @@
+import os
 class DiceRoller():
     def startup(self):
-    	print 'Hello Ash'
+    	ESCAPE = u"\u001B"
+    	os.system("echo {}[31mHello World{}[0m".format(ESCAPE, ESCAPE))
+
+
 
 d = DiceRoller()
 d.startup()
